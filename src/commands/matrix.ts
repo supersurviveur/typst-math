7141fc9faa3dffc25d5267228e48a995070dd62e
Surviveur <julien.thillard38@gmail.com>
2024-01-12 07:45:47 +0100

    Refactor `matrix` command

--- src/commands/matrix.ts
+++ src/commands/matrix.ts
@@ -1,5 +1,23 @@
 import * as vscode from 'vscode';
 
+function generateMatrixSnippet(height: number, width: number) {
+    let snippet = "mat(\n";
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            snippet += `\${${i * width + j + 1}:0}`;
+            if (j < width - 1) {
+                snippet += ", ";
+            }
+        }
+        if (i < height - 1) {
+            snippet += ";\n";
+        }
+    }
+    snippet += ")";
+
+    return new vscode.SnippetString(snippet);
+}
+
 export const matrixCommand = vscode.commands.registerCommand('typst-math.matrix', async () => {
     // Get matrix height
     let value = await vscode.window.showInputBox({
@@ -19,32 +37,20 @@ export const matrixCommand = vscode.commands.registerCommand('typst-math.matrix'
     // Get matrix width
     value = await vscode.window.showInputBox({
         prompt: "Matrix width",
-        placeHolder: "3"
+        placeHolder: "3",
+        validateInput: (value: string) => {
+            let height = parseInt(value);
+            if (isNaN(height)) {
+                return "Invalid width";
+            }
+            return undefined;
+        },
     });
     if (value === undefined) { return; }
     let width = parseInt(value);
-    if (isNaN(width)) {
-        vscode.window.showErrorMessage("Invalid width");
-        return;
-    }
-
-    // Generate the matrix snippet
-    let snippet = "mat(\n";
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            snippet += `\${${i * width + j + 1}:0}`;
-            if (j < width - 1) {
-                snippet += ", ";
-            }
-        }
-        if (i < height - 1) {
-            snippet += ";\n";
-        }
-    }
-    snippet += ")";
 
     // Insert the snippet
     let editor = vscode.window.activeTextEditor;
     if (editor === undefined) { return; }
-    editor.insertSnippet(new vscode.SnippetString(snippet), editor.selection);
+    editor.insertSnippet(generateMatrixSnippet(width, height), editor.selection);
 });
