da3aa3469b91997a66b4ba1a2a84b95c039bba25
supersurviveur <julien.thillard38@gmail.com>
2024-05-26 11:02:02 +0200

    Add tests coverage script

---
+++ typst-math-rust/coverage.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Generate profile files
+RUSTFLAGS="-C instrument-coverage" cargo test --tests 
+cargo profdata -- merge -sparse default_*.profraw -o typst-math.profdata
+
+# Get binary names
+FILES=$( \
+      for file in \
+        $( \
+          RUSTFLAGS="-C instrument-coverage" \
+            cargo test --tests --no-run --message-format=json \
+              | jq -r "select(.profile.test == true) | .filenames[]" \
+              | grep -v dSYM - \
+        ); \
+      do \
+        printf "%s %s " -object $file; \
+      done \
+    )
+
+# Generate report
+cargo cov -- report \
+    $FILES \
+    --use-color --ignore-filename-regex='/.cargo/registry' \
+    --instr-profile=typst-math.profdata
+
+# Show where code isn't covered 
+# cargo cov -- show \
+#     $FILES \
+#     --use-color --ignore-filename-regex='/.cargo/registry' \
+#     --instr-profile=typst-math.profdata --summary-only \
+#     --show-instantiations --show-line-counts-or-regions \
+#     --Xdemangler=rustfilt | less -R
+
+# Clean files
+rm -f default_*.profraw
+rm -f typst-math.profdata
